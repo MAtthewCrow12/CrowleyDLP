4cfba22749c560d52425f190be5a466aa0fa2415
MAtthewCrow12 <34295199+MAtthewCrow12@users.noreply.github.com>
2019-06-16 14:47:24 -0700

    functional subroutine
    subroutine works in test but the drip searches start at 1 and am unable to change where it begins to itterate.

--- test.f90
+++ test.f90
@@ -1,36 +1,91 @@
 program test
 implicit none
-integer :: n, z, ZDnrange
+integer :: n, z, ZDnrange, NDzrange
 integer, allocatable :: zdripndep(:)
-
-
+integer, allocatable :: ndripzdep(:)
+real,    allocatable :: BindingEarr(:,:)
 open(unit=20,  file="Zdrip.dat",                status="unknown")
 open(unit=21,  file="Zdripsearch.dat",          status="unknown")
+open(unit=22,  file="Ndrip.dat",                status="unknown")
+open(unit=23,  file="Ndripsearch.dat",          status="unknown")
+open(unit=24,  file="BindingEnergy.dat",        status="unknown")
+
+
 
 write(*, "(A)", advance="NO") "desired n range for protron drip: nfinz=" 
 read *, ZDnrange
 
+
+
 allocate (zdripndep(ZDnrange))
 call ProtonDripLine(ZDnrange, zdripndep)
-!call NeutronDripLine()
+!NDzrange=zdripndep(ZDnrange)
+!allocate (ndripzdep(NDzrange))
+!call NeutronDripLine(NDzrange, ndripzdep)
+
+
+!call BindingEnergy(ZDnrange)
+
 
 end program test
 
 
 
 
+
+subroutine BindingEnergy(ZDnrange)
+implicit none 
+integer :: n, z, ZDnrange, NDzrange
+integer, allocatable :: zdripndep(:)
+integer, allocatable :: ndripzdep(:)
+real,    allocatable :: BindingEarr(:,:)
+
+allocate (zdripndep(ZDnrange))
+call ProtonDripLine(ZDnrange, zdripndep)
+NDzrange=zdripndep(ZDnrange)
+allocate (ndripzdep(NDzrange))
+call NeutronDripLine(NDzrange, ndripzdep)
+allocate (BindingEarr(NDzrange, ZDnrange))
+
+do n=1, ZDnrange
+   do z=1, NDzrange
+       if(z<=zdripndep(n) .and. n<=ndripzdep(z)) then
+       write(24, *) n, z !Binding Energy test
+        call BindingE(z, n, BindingEarr(z,n))
+        end if
+    end do
+end do
+
+do n=1, ZDnrange
+    do z=1, NDzrange
+
+    if(z<=zdripndep(n) .and. n<=ndripzdep(z)) then
+    print *, "n=", n, "z=", z, "BE=", BindingEarr(z, n)
+    end if
+
+   end do
+end do
+
+
+print *, "ZDnrange=", ZDnrange, "NDzrange=", NDzrange
+
+
+end subroutine BindingEnergy
+
+
+
+
+
+
 subroutine ProtonDripLine(ZDnrange, zdripndep)
 implicit none
-integer :: n, z, temp
+integer :: n, z
 integer :: ZDnrange, ZDzrange
 integer :: zdripndep(ZDnrange)
 real    :: bez(4*ZDnrange)
 real    :: sez
 
-!============================================
-!============================================
-!============================================
-do n=1, ZDnrange
+do n=2, ZDnrange
 
     do z=1, zdripndep(n-1)+10
         call BindingE(z,n,bez(z))
@@ -46,19 +101,47 @@ do n=1, ZDnrange
         end if 
     end do  
 
-
 end do
-!============================================
-!============================================
-!============================================
 do n=1, ZDnrange !SEC 1 
     write(20,*) n, zdripndep(n)
-    print *,  n, zdripndep(n)
+    print *,    "Proton Drip:   ", "n=", n, " z=", zdripndep(n)
 end do 
 
 end subroutine ProtonDripLine
 
 
+subroutine NeutronDripLine(NDzrange, ndripzdep)
+implicit none
+integer :: n, z
+integer :: NDnrange, NDzrange
+integer :: ndripzdep(NDzrange)
+real    :: ben(4*NDzrange)
+real    :: sen
+
+do z=1, NDzrange
+
+    do n=1, ndripzdep(z-1)+10
+        call BindingE(z,n,ben(n))
+        write(23, *) n, z
+    end do
+
+
+    do n=1,  ndripzdep(z-1)+10
+        sen=ben(n+1)-ben(n)
+        if (sen<0.) then 
+        ndripzdep(z)=n
+        exit       
+        end if 
+    end do  
+
+end do
+
+do z=1, NDzrange !SEC 1 
+    write(22,*) ndripzdep(z), z
+    print *,    "Neutron Drip: ", " z=", z, " n=", ndripzdep(z)
+end do 
+
+end subroutine NeutronDripLine
 
 
 
@@ -69,24 +152,6 @@ end subroutine ProtonDripLine
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-!-------------------------------------
-!-------------------------------------
-!-------------------------------------
-!-------------------------------------
-!-------------------------------------
 !subroutine to find the binding energy
 subroutine BindingE(z,n,be)
 implicit none
@@ -102,24 +167,25 @@ p3=-(0.72*(Z*(Z-1.))*(A**(-1./3.)))
 p4= -(23.*((Z-N)**2.))/A
 !||||||||||||||||||||||||||||||||||
 if(    (mod(z,2)==0)  ) then
-if(    (mod(n,2)==0)  ) then
-p5=34.*(a**(-3./4.))
-!print *, "p5 z,n= even"
-end if
-if(    (mod(n,2)==1)  ) then
-p5=0
-!a is odd
-end if
+    if(    (mod(n,2)==0)  ) then
+        p5=34.*(a**(-3./4.))
+        !print *, "p5 z,n= even"
+    end if
+    if(    (mod(n,2)==1)  ) then
+        p5=0
+        !a is odd
+    end if
 end if !main if
+
 if(    (mod(z,2)==1)  ) then
-if(    (mod(n,2)==1)  ) then
-p5=-34.*(a**(-3./4.))
-!print *, "p5 z,n= odd"
-end if
-if(    (mod(n,2)==0)  ) then
-p5=0
-!a is odd
-end if
+    if(    (mod(n,2)==1)  ) then
+        p5=-34.*(a**(-3./4.))
+        !print *, "p5 z,n= odd"
+    end if
+    if(    (mod(n,2)==0)  ) then
+        p5=0
+        !a is odd
+    end if
 end if !main if
 !p5=0 !ignoring P5
 !-----------------------------
